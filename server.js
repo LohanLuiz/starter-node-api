5908a44774c1c7eb1083a8e2ec1acbdc4411ad18
Lohan Luiz <lohan.luiz@emdurportovelho.com>
2020-08-19 10:43:03 -0400

    Connect to Database

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 // Import module express.
-const express = require('express');
+const express = require("express");
+
+// Import Mongoose;
+const mongoose = require('mongoose');
 
 // Define a variable with the express function.
+// Initialize the Express module;
 const app = express();
+
+// Initializing Database
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true})
 
 // Create a route for the application, root Route is = "/".
 // Parameter "req" = Requisition to server.
 // Parameter "res" = Response to client.
+// This is the first route.
 app.get('/', (req, res) => {
   res.send('Hello Rocketseat');
 });
